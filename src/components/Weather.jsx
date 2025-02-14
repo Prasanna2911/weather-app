8ea8ceddbb366181c429839d98337d5f5ae10446
Prasanna2911 <bpp1757@gmail.com>
2025-02-14 23:43:45 +0530

    design updated

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,4 +1,13 @@
 import search from "../assets/search.png";
+import clear from "../assets/clear.png";
+import cloud from "../assets/cloud.png";
+import drizle from "../assets/drizzle.png";
+import rain from "../assets/rain.png";
+import snow from "../assets/snow.png";
+import wind from "../assets/wind.png";
+import humidity from "../assets/humidity.png";
+import "../components/Weather.css";
+
 const Weather = () => {
   return (
     <div className="weather place-self-center p-[40px] rounded-[10px] bg-linear-45 from-[#2f4680] to-[#500ae4] flex flex-col items-center">
@@ -13,6 +22,26 @@ const Weather = () => {
           alt="Search-Icon"
           className="w-[50px] cursor-pointer p-[16px] rounded-[50%] bg-[#ebfffc]"
         />
+      </div>
+      <img src={clear} alt="" className="w-[150px] mx-0 my-[10px]" />
+      <p className="text-white text-[80px] leading-none">16°c</p>
+      <p className="location text-[40px] text-white">London</p>
+      {/* Weathear Data */}
+      <div className="weather-data flex  justify-between mt-8">
+        <div className="col ">
+          <img src={humidity} alt="" />
+          <div>
+            <p>91 %</p>
+            <span>Humidity</span>
+          </div>
+        </div>
+        <div className="col">
+          <img src={wind} alt="" />
+          <div>
+            <p>3.6 km</p>
+            <span>Wind</span>
+          </div>
+        </div>
       </div>
     </div>
   );
